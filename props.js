4494a098c8b74066e9ac3341b2c9ebae78ee425f
cartern206 <cartern206@gmail.com>
2020-01-27 21:37:04 -0700

    prop format

--- props.js
+++ props.js
@@ -3,127 +3,127 @@
 var data = {
    "props": [
      {
-     "Name": "Team to Score First",
+     "Name": "Team to Score First:",
      "A": "Chiefs",
      "B": "49ers"
    }, 
    {
-    "Name": "Gatorade Color",
+    "Name": "Gatorade Color:",
     "A": "Red",
     "B": "Blue"
    }, 
    {
-    "Name": "Throws an Incterception",
+    "Name": "Throws an Interception:",
     "A": "Chiefs",
     "B": "49ers"
    },
    {
-    "Name": "Field goal in the first quarter",
+    "Name": "Field goal in the first quarter:",
     "A": "Yes",
     "B": "No"
    },
    {
-    "Name": "First half Touchdown!",
+    "Name": "First half Touchdown:",
     "A": "Chiefs",
     "B": "49ers"
    },
    {
-    "Name": "Shortest Touch Down",
+    "Name": "Shortest Touch Down:",
     "A": "OVER 1.5yrds",
     "B": "Under 1.5yrds"
    },
    {
-    "Name": "Total Field Goals made in game",
+    "Name": "Total Field Goals made in game:",
     "A": "OVER 3.5",
     "B": "Under 3.5"
    },
    {
-    "Name": "Longest Touch Down",
+    "Name": "Longest Touch Down:",
     "A": "OVER 47.5yrds",
     "B": "Under 47.5yrds"
    },
    {
-    "Name": "First touchdown a passing touchdown",
+    "Name": "First touchdown is a passing touchdown:",
     "A": "Yes",
     "B": "No"
    },
    {
-    "Name": "Largest lead of the game",
+    "Name": "Largest lead of the game:",
     "A": "OVER 14.5pts",
     "B": "Under 14.5pts"
    },
    {
-    "Name": "Second Half Touchdown!",
+    "Name": "Second Half Touchdown:",
     "A": "Chiefs",
     "B": "49ers"
    },
    {
-    "Name": "Total Interceptions",
+    "Name": "Total Interceptions:",
     "A": "OVER 1.5",
     "B": "Under 1.5"
    },
    {
-    "Name": "Total Jimmy Garoppolo passing",
+    "Name": "Total Jimmy Garoppolo passing:",
     "A": "OVER 239.5yrds",
     "B": "Under 239.5yrds"
    },
    {
-    "Name": "Total Jimmy Garoppolo rushing",
+    "Name": "Total Jimmy Garoppolo rushing:",
     "A": "OVER 3.5yrds",
     "B": "Under 3.5yrds"
    },
    {
-    "Name": "Will the game be Tied after 0-0",
+    "Name": "Will the game be Tied? (after 0-0)",
     "A": "Yes",
     "B": "No"
    },  
     {
-    "Name": "Coaches Challenge",
+    "Name": "Coach's Challenge:",
     "A": "Chiefs",
     "B": "49ers"
    },
    {
-    "Name": "Defense with the most Sacks",
+    "Name": "Defense with the most Sacks:",
     "A": "Chiefs",
     "B": "49ers"
    },
    {
-    "Name": "First Half Score",
-    "A": "Chiefs -1.5pts",
-    "B": "49ers +1.5pts"
+    "Name": "First Half Leader:",
+    "A": "Chiefs",
+    "B": "49ers/Tie"
    },
    {
     "Name": "What will happen first?",
     "A": "Score",
     "B": "Punt"
-   },
+   },S
    {
-    "Name": "Convert on 4th down",
+    "Name": "Convert on 4th down:",
     "A": "Yes",
     "B": "No"
    },
    {
-    "Name": "Total Patrick Mahomes passing",
+    "Name": "Total Patrick Mahomes passing:",
     "A": "OVER 308.5yrds",
     "B": "Under 308.5yrds"
    },
    {
-    "Name": "Total Patrick Mahomes rushing",
+    "Name": "Total Patrick Mahomes rushing:",
     "A": "OVER 30.5yrds",
     "B": "Under 30.5yrds"
    },
    {
-    "Name": "Total Tyreek Hill recieving",
+    "Name": "Total Tyreek Hill recieving:",
     "A": "OVER 73.5yrds",
     "B": "Under 73.5yrds"
    },
    {
-    "Name": "Jersey Number of Player to Score First Touchdown",
+    "Name": "Jersey Number of Player to Score First Touchdown:",
     "A": "OVER 26.5",
     "B": "Under 26.5"
    },
    {
-    "Name": "First Half Total Points",
+    "Name": "First Half Total Points:",
     "A": "OVER 27.5",
     "B": "Under 27.5"
    },
